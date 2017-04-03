5067a73659a5574ddbdbb41d29db4131e6ec8e1d
glauberrleite <contact@glauberrleite.com>
2017-04-02 21:39:56 -0300

    Added random initial values to weights

--- mlp.cpp
+++ mlp.cpp
@@ -1,4 +1,6 @@
 #include"mlp.h"
+#include<random>
+#include<algorithm>
 
 float MLP::sigmoid(float x) {
   return 1/(1 + exp(-x));
@@ -15,26 +17,52 @@ MLP::MLP(int numberOfHiddenLayers, int numberOfHiddenNeurons, int numberOfClasse
   // As we have 3 possible classes, there are 3 outputs
   this->outputs.resize(numberOfClasses);
 
+  // Pseudo Random generator
+  std::mt19937 randomGenerator;
+  std::uniform_int_distribution<std::mt19937::result_type> dist(0,1);
+  randomGenerator.seed(std::random_device()());
+
+  /* The anatomy of weights
+  // weights[i][j][k] where:
+  // i is the layer
+  // j is the output neuron
+  // k is the input neuron
+  */
+
+
+  // Building weights matrix and filling it with random values
   this->weights = new float**[numberOfHiddenLayers + 2];
 
   // In the first layer we have 4 weights, for the four variables
   this->weights[0] = new float*[numberOfHiddenNeurons];
-  for (int j = 0; j < numberOfHiddenNeurons; ++j)
+  for (int j = 0; j < numberOfHiddenNeurons; ++j){
     this->weights[0][j] = new float[4];
+
+    for (int k = 0; k < 4; ++k)
+      this->weights[0][j][k] = dist(randomGenerator);
+  }
 
 
   // The rest of the layers, except output, use the layer before as input
   for (int i = 1; i <= numberOfHiddenLayers; ++i) {
     this->weights[i] = new float*[numberOfHiddenNeurons];
 
-    for (int j = 0; j < numberOfHiddenNeurons; ++j)
+    for (int j = 0; j < numberOfHiddenNeurons; ++j){
       this->weights[i][j] = new float[numberOfHiddenNeurons];
+
+      for (int k = 0; k < numberOfHiddenNeurons; ++k)
+        this->weights[i][j][k] = dist(randomGenerator);
+    }
   }
 
   // The last layer is the layer of outputs
   this->weights[numberOfHiddenLayers + 1] = new float*[outputs.size()];
   for (int j = 0; j < outputs.size(); ++j) {
-    this->weights[numberOfHiddenLayers + 1][j] = new float[numberOfHiddenLayers];
+    this->weights[numberOfHiddenLayers + 1][j] = new float[numberOfHiddenNeurons];
+
+    for(int k = 0; k < numberOfHiddenNeurons; ++k)
+      this->weights[numberOfHiddenLayers + 1][j][k] = dist(randomGenerator);
+
   }
 
 }
@@ -68,7 +96,9 @@ int MLP::classificate(float sepalLength, float sepalWidth, float petalLength, fl
     }
   }
 
-  // Now, the output vector gets the last hidden neurons as input
+  // Now, the output vector gets the last hidden neurons as input, and the max value is held
+  int max = 0;
+  int result = 0;
   for (int i = 0; i < outputs.size(); i++){
     outputs[i] = 0;
 
@@ -77,7 +107,12 @@ int MLP::classificate(float sepalLength, float sepalWidth, float petalLength, fl
     }
 
     outputs[i] = sigmoid(outputs[i]);
+
+    if(outputs[i] > max)
+      result = i;
   }
+
+  return result;
 
 }
 
